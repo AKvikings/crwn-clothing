6dccb00b5ec36acbd04600139b1466e64ccae8d0
AKVikings <ajithkumarvijay143@gmail.com>
2021-04-10 11:25:20 +0530

    first commit of ecommerce app. completed menu item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory-component";
+
+import './homepage.styles.scss';
+
+
+
+export const Homepage = () => (
+    <div className='Homepage'>
+        <Directory />
+    </div>
+);
+
